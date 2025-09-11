4df0318b5ab217eba97febe379d87b8d833c37f3
Arnaud <arnaud.obri@proton.me>
2025-09-11 12:19:20 +0200

    ctc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     default: "Arnaud's Personal Website - arnaudobri.com",
     template: "%s - arnaudobri.com",
   },
-  description: "Welcome to my personal website",
+  description: "Welcome to personal website",
   twitter: {
     card: "summary_large_image",
   },
